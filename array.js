dd77f92c81c6775051fe22d52532c773fbcb5ade
Saddeeq84 <musaddiqun@gmail.com>
2024-11-25 22:27:55 +0100

    Delete array.js

--- array.js
+++
@@ -1,9 +0,0 @@
-var cars =['toyota', 'honda', 'mazda', 'mitsubishi', 'tesla', 'lexus'];
-console.log(cars[0]);
-console.log(cars[cars.length -1]);
-cars.push('Mercedes-Benz');
-console.log(cars);
-for(var car of cars){
-    console.log(cars);
-}
-
