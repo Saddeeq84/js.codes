8ab3e8b9be2f28bb950b8ab0748508339efe8324
Saddeeq84 <musaddiqun@gmail.com>
2024-11-27 05:59:38 +0100

    Add files via upload

---
+++ car-listing.js
@@ -0,0 +1,7 @@
+var cars =['toyota', 'honda', 'mazda', 'mitsubishi', 'tesla', 'lexus'];
+console.log(cars[0]);
+console.log(cars[cars.length -1]);
+cars.push('Mercedes-Benz');
+console.log(cars);
+for(var car =0; car < cars.length; cars++){
+    console.log(cars);}
